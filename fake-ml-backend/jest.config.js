3a904f2592824b1005581eecfc1516b21809ceba
Charly <magicstyl3@gmail.com>
2025-05-29 19:29:43 -0300

    fix!: Changes on heriarchy folders

--- fake-ml-backend/jest.config.js
+++ fake-ml-backend/jest.config.js
@@ -1,33 +1,16 @@
 module.exports = {
-  // Extensiones de archivo de test
+  rootDir: '.', // ya que el WD es fake-ml-backend
   moduleFileExtensions: ['js', 'json', 'ts'],
-  // Raíz de tu proyecto
-  rootDir: '.',
-  // Regex para reconocer tus .spec.ts y .test.ts
   testRegex: '\\.(spec|test)\\.ts$',
-  // Transpila con ts-jest
   transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
+    '^.+\\.(ts|js)$': 'ts-jest',
   },
-  // Genera coverage
   collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    // excluye entrypoint y módulos
-    '!src/main.ts',
-    '!src/**/*.module.ts',
-  ],
+  collectCoverageFrom: ['src/**/*.ts', '!src/main.ts', '!src/**/*.module.ts'],
   coverageDirectory: 'coverage',
-  // Umbrales mínimos de cobertura
   coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
+    global: { branches: 80, functions: 80, lines: 80, statements: 80 },
   },
-  // Entorno de Node para tus tests
   testEnvironment: 'node',
   globals: {
     'ts-jest': {
